1114f319c0d8c990aa702e5af8ea61c5325d1df1
Siren <yechengjian@xgd.com>
2025-12-31 13:15:03 +0800

    commit: adaptation

--- src/com/android/documentsui/roots/ProvidersCache.java
+++ src/com/android/documentsui/roots/ProvidersCache.java
@@ -207,7 +207,8 @@ public class ProvidersCache implements ProvidersAccess, LookupApplicationName {
             assert (recentRoot.rootId == null);
             assert (recentRoot.derivedIcon == R.drawable.ic_root_recent);
             assert (recentRoot.derivedType == RootInfo.TYPE_RECENTS);
-            assert (recentRoot.flags == (Root.FLAG_LOCAL_ONLY | Root.FLAG_SUPPORTS_IS_CHILD));
+            // 调试模式下会奔溃，注释掉这句断言
+            // assert (recentRoot.flags == (Root.FLAG_LOCAL_ONLY | Root.FLAG_SUPPORTS_IS_CHILD));
             assert (recentRoot.availableBytes == -1);
         }
 
